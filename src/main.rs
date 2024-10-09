9202ec8ad4529e265292e5f707969df8c042fcb2
cocoman99 <cocoman99@o2.pl>
2024-10-09 22:43:10 +0200

    Changing main.rs to my custom example

--- src/main.rs
+++ src/main.rs
@@ -7,28 +7,10 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    // create a list of 5 signals
-    let length = 5;
-    let counters = (1..=length).map(|idx| create_signal(idx));
-
-    // each item manages a reactive view
-    // but the list itself will never change
-    let counter_buttons = counters
-        .map(|(count, set_count)| {
-            view! {
-                <li>
-                    <button
-                        on:click=move |_| set_count.update(|n| *n += 10)
-                    >
-                        {count}
-                    </button>
-                </li>
-            }
-        })
-        .collect_view();
 
     view! {
-        <ul>{counter_buttons}</ul>
+        <h1 style="background-color:#a6edc1;">GUESS-THE-NUMBER</h1>
+        
     }
 }
 
