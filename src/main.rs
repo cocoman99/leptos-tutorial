25cbad6f35cd74413d7eed69a0e0f64b8322b41a
cocoman99 <cocoman99@o2.pl>
2024-10-10 22:34:40 +0200

    Expanding app. Styling.

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,31 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
 
     view! {
-        <h1 style="background-color:#a6edc1;">GUESS-THE-NUMBER</h1>
-        
+        <h1 style="background-color:#a6edc1;">* * * GUESS-THE-NUMBER * * *</h1>
+            <div class="main">        
+                <div>"CHOOSE NUMBER FROM 1 TO 100."</div>
+                <div>
+                "INPUT YOUR GUESS "<button
+                    on:click=move |_| {
+                        // on stable, this is set_count.set(3);
+                        set_count.set(3);
+                    }
+                    >
+                    "HERE"
+                </button>                
+                </div>
+                "THEN CLICK "
+                <button
+                    on:click=move |_| {
+                        // on stable, this is set_count.set(3);
+                        set_count.set(3);
+                    }
+                    >
+                    "HERE"
+                </button>" TO CHECK"
+            </div>
     }
 }
-
